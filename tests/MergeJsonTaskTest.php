34361c2d52cc69c2969f928c00ab33cc79959853
Toby Mackenzie <public@tobymackenzie.com>
2022-11-21 15:51:57 -0500

    tests: add another variation for json merge

--- tests/MergeJsonTaskTest.php
+++ tests/MergeJsonTaskTest.php
@@ -11,4 +11,12 @@ class MergeJsonTaskTest extends TestCase{
 		], $outFile))->do();
 		$this->assertEquals(file_get_contents($this->getDataPath('expect/composer.json')), file_get_contents($outFile));
 	}
+	public function testIntoCopiedFile(){
+		$outFile = $this->getTmpPath('/composer.json');
+		exec("cp -a {$this->getDataPath('one/composer.json')} {$outFile}");
+		(new MergeJsonTask([
+			$this->getDataPath('two/composer.json'),
+		], $outFile))->do();
+		$this->assertEquals(file_get_contents($this->getDataPath('expect/composer.json')), file_get_contents($outFile));
+	}
 }
